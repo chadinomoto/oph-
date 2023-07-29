82cbc72b1f135c212480d0aced33399fa877f9ad
Chainxc <supawatta.rat_g31@mwit.ac.th>
2023-07-29 14:49:02 +0700

    bg changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -194,20 +194,20 @@ export default function Home({ userData }) {
         />
       </Head>
       <div className='flex flex-col w-full'>
-        <div
+        {/*<div
           style={{
             backgroundImage: `url(${process.env.CDN_URL + '/img/sky.webp'})`,
           }}
           className={
             'w-full bg-cover bg-bottom sm:bg-fixed flex items-end min-h-screen relative justify-self-center self-center space-y-2'
           }
-        >
-          {/* <div
+        >*/}
+        <div
             style={{
               backgroundImage: `url(${'' + '/img/welcome.webp'})`,
             }}
             className='block fixed inset-0 bg-cover -z-10'
-          /> */}
+        />
           <div className='w-full h-full py-6 flex items-center'>
             <div className='w-full max-w-7xl grid md:grid-cols-2 gap-6 items-center justify-items-center mx-auto px-6'>
               <iframe
