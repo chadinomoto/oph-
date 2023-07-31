dd9826aea30ab003366705840885c948e37ecc75
Chainxc <supawatta.rat_g31@mwit.ac.th>
2023-07-31 21:58:26 +0700

    func

--- pages/index.jsx
+++ pages/index.jsx
@@ -156,18 +156,17 @@ export default function Home({ userData }) {
       ref: subjRef,
     },
   ]*/
-  let slideIndex = [1,1];
-  let slideId = ["mySlides1", "mySlides2"]
-  showSlides(1, 0);
-  showSlides(1, 1);
-
+  const slideIndex = [1,1]
+  const slideId = ["mySlides1", "mySlides2"]
+  showSlides(1, 0)
+  showSlides(1, 1)
   function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
   }
 
   function showSlides(n, no) {
-    let i;
-    let x = document.getElementsByClassName(slideId[no]);
+    const i=0
+    const x = document.getElementsByClassName(slideId[no]);
     if (n > x.length) {slideIndex[no] = 1}    
     if (n < 1) {slideIndex[no] = x.length}
     for (i = 0; i < x.length; i++) {
