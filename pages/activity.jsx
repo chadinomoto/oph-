138e70175d4e2e2f72d4e207cbc97f6d69e5383e
Chainxc <supawatta.rat_g31@mwit.ac.th>
2023-07-31 10:59:43 +0700

    bg

--- pages/activity.jsx
+++ pages/activity.jsx
@@ -71,19 +71,17 @@ export default function Activity() {
           content={'https://mwitophcdn.woyiswoy.com/img/' + meta.img}
         />
     </Head>
-        <main className='flex flex-col items-center w-full max-w-7xl px-8 py-6 gap-4 mx-auto'>
-            <span
+        <main style={{
+              backgroundImage: `url(${'' + '/img/sky.webp'})`,
+            }}
+            className='w-full bg-cover bg-bottom sm:bg-fixed flex items-end min-h-screen relative justify-self-center self-center space-y-2 '
+        >
+          <div className='w-full h-full py-6 flex items-center'>
+          <span
               className='font-CS font-bold text-2xl md:text-3xl lg:text-4xl pt-16'
             >
               Activities
             </span>
-            <div
-            style={{
-              backgroundImage: `url(${'' + '/img/sky.webp'})`,
-            }}
-            className='w-full bg-cover bg-bottom sm:bg-fixed flex items-end min-h-screen relative justify-self-center self-center space-y-2'
-        >
-          <div className='w-full h-full py-6 flex items-center'>
             <div className='w-full max-w-7xl grid md:grid-cols-2 gap-6 items-center justify-items-center mx-auto px-6'>
               <iframe
                 type='text/html'
@@ -196,7 +194,6 @@ export default function Activity() {
               </div>
               </div>
             </div>
-          </div>
         </main>
     </>
       
