92c385c68e6c48801d3c042429712ac261c3ee57
Chainxc <supawatta.rat_g31@mwit.ac.th>
2023-08-01 21:58:34 +0700

    find Unused file at self/z-Unused

--- pages/index.jsx
+++ pages/index.jsx
@@ -332,10 +332,10 @@ export default function Home({ userData }) {
                 >
                 </div>
                 <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                  <FontAwesomeIcon icon={faChevronLeft} onClick={prevSlide} size={30} />
+                  <FontAwesomeIcon icon={faChevronLeft} onClick={prevSlide} size='sm' />
                 </div>
                 <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                  <FontAwesomeIcon icon={faChevronRight} onClick={nextSlide} size={30} />
+                  <FontAwesomeIcon icon={faChevronRight} onClick={nextSlide} size='sm' />
                 </div>
                 <div className='flex top-4 justify-center py-2'>
                   {slides.map((slide, slideIndex) => (
@@ -509,11 +509,11 @@ export default function Home({ userData }) {
                     </div>
                     {/* Left Arrow */}
                     <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                      <FontAwesomeIcon icon={faChevronLeft} onClick={prevSlide1} size={30} />
+                      <FontAwesomeIcon icon={faChevronLeft} onClick={prevSlide1} size='sm' />
                     </div>
                     {/* Right Arrow */}
                     <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                      <FontAwesomeIcon icon={faChevronRight} onClick={nextSlide1} size={30} />
+                      <FontAwesomeIcon icon={faChevronRight} onClick={nextSlide1} size='sm' />
                     </div>
                     <div className='flex top-4 justify-center py-2'>
                       {slides1.map((slide, slideIndex) => (
@@ -542,11 +542,11 @@ export default function Home({ userData }) {
                     </div>
                     {/* Left Arrow */}
                     <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                      <FontAwesomeIcon icon={faChevronLeft} onClick={prevSlide2} size={30} />
+                      <FontAwesomeIcon icon={faChevronLeft} onClick={prevSlide2} size='sm' />
                     </div>
                     {/* Right Arrow */}
                     <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-                      <FontAwesomeIcon icon={faChevronRight} onClick={nextSlide2} size={30} />
+                      <FontAwesomeIcon icon={faChevronRight} onClick={nextSlide2} size='sm' />
                     </div>
                     <div className='flex top-4 justify-center py-2'>
                       {slides2.map((slide, slideIndex) => (
