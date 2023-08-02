36e5d047f89a9f141e9919b3be32eae3eb567950
Chainxc <supawatta.rat_g31@mwit.ac.th>
2023-08-02 11:22:59 +0700

    Oita

--- pages/index.jsx
+++ pages/index.jsx
@@ -316,15 +316,11 @@ export default function Home({ userData }) {
             'w-full bg-cover bg-bottom sm:bg-fixed flex items-end min-h-screen relative justify-self-center self-center space-y-2'
           }
         >*/}
-        <div
-          style={{
-            backgroundImage: '/img/sky.webp',
-          }}
-          className='w-full bg-cover bg-bottom flex items-end min-h-screen relative justify-self-center self-center space-y-2'
+        <div className='w-full bg-cover bg-[url(/img/sky2023.webp)] bg-bottom flex items-end min-h-screen relative justify-self-center self-center space-y-2'
         >
           <div className='w-full h-full py-6 flex items-center'>
             <div className='w-full max-w-7xl grid md:grid-cols-2 gap-6 items-center justify-items-center mx-auto px-6'>
-              <div className='max-w-[640px] max-h-[480px] w-[16rem] md:w-[32rem] m-auto py-16 px-4 relative group'>
+              <div className='max-w-[640px] max-h-[480px] w-[16rem] md:w-[32rem] h-[12rem] md:h-[24rem] m-auto py-16 px-4 relative group'>
                 <img src={slides[currentIndex].url} className='w-full h-full rounded-2xl bg-center bg-cover duration-300'/>
                 <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
                   <FontAwesomeIcon icon={faChevronLeft} onClick={prevSlide} size='sm' />
@@ -485,7 +481,7 @@ export default function Home({ userData }) {
                   SQUARE&CHALLENGE
                 </div>
                 <div>
-                  <div className='max-w-[640px] max-h-[480px] h-screen w-[16rem] md:w-[32rem] m-auto py-16 px-4 relative group'>
+                  <div className='max-w-[640px] max-h-[480px] h-screen w-[16rem] md:w-[32rem] h-[12rem] md:h-[24rem] m-auto py-16 px-4 relative group'>
                     <img src={slides1[currentIndex1].url} className='w-full h-full rounded-2xl bg-center bg-cover duration-300'/>
                     {/* Left Arrow */}
                     <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
@@ -514,7 +510,7 @@ export default function Home({ userData }) {
                   กิจกรรมตามสาขาวิชา&CLUBFES
                 </div>
                 <div>
-                  <div className='max-w-[640px] max-h-[480px] h-screen w-[16rem] md:w-[32rem] m-auto py-16 px-4 relative group'>
+                  <div className='max-w-[640px] max-h-[480px] h-screen w-[16rem] md:w-[32rem] h-[12rem] md:h-[24rem]s m-auto py-16 px-4 relative group'>
                     <img src={slides2[currentIndex2].url} className='w-full h-full rounded-2xl bg-center bg-cover duration-300'/>
                     {/* Left Arrow */}
                     <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
